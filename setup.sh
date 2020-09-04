7d38974c3b606b4fb4f97d0d6e1d7dff40d50a68
Ayoma Wijethunga <ayomawdb@gmail.com>
2020-09-04 09:28:43 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cd ~
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update -y && sudo apt-get upgrade -y 
 
-sudo apt-get install cmake libjpeg8-dev git
+sudo apt-get install -y cmake libjpeg8-dev git
 
 mkdir ~/lili
 cd ~/lili
@@ -17,21 +17,21 @@ cd ../../
 
 # Based on: https://www.pyimagesearch.com/2019/09/16/install-opencv-4-on-raspberry-pi-4-and-raspbian-buster/
 
-sudo apt-get install build-essential cmake pkg-config
-sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install -y build-essential cmake pkg-config
+sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
 
-sudo apt-get install libfontconfig1-dev libcairo2-dev
-sudo apt-get install libgdk-pixbuf2.0-dev libpango1.0-dev
-sudo apt-get install libgtk2.0-dev libgtk-3-dev
+sudo apt-get install -y libfontconfig1-dev libcairo2-dev
+sudo apt-get install -y libgdk-pixbuf2.0-dev libpango1.0-dev
+sudo apt-get install -y libgtk2.0-dev libgtk-3-dev
 
-sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install -y libatlas-base-dev gfortran
 
-sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-103
-sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+sudo apt-get install -y libhdf5-dev libhdf5-serial-dev libhdf5-103
+sudo apt-get install -y libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
 
-sudo apt-get install python3-dev
+sudo apt-get install -y python3-dev
 
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
